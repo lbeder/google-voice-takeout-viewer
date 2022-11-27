1509f8a8d3be1328654c0cca7f7e2bf9800e2280
lbeder <lbeder@users.noreply.github.com>
2022-11-27 23:46:03 +0000

    Support multiple files

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,35 +3,71 @@ import './Dashboard.scss';
 import DataTable from './DataTable';
 import { ParseResult } from 'papaparse';
 import { useState } from 'react';
-import { Container, Modal } from 'react-bootstrap';
+import { Container, ListGroup, Modal } from 'react-bootstrap';
 
 const Dashboard = () => {
   const [data, setData] = useState<Entry[]>([]);
+  const [files, setFiles] = useState<string[]>([]);
   const [error, setError] = useState('');
 
-  const onUploadAccepted = (results: ParseResult<string[]>) => {
-    setData(results.data.filter((r) => Object.keys(r).length > 1).map((r) => new Entry(r)));
+  const onUploadAccepted = (results: ParseResult<string[]>, file?: File) => {
+    if (file?.name) {
+      const { name } = file;
+
+      setFiles([name, ...files]);
+    }
+
+    const newData = [...data, ...results.data.filter((r) => Object.keys(r).length > 1).map((r) => new Entry(r))];
+
+    setData(newData);
+  };
+
+  const validator = (file: File) => {
+    if (files.includes(file.name)) {
+      setError(`File ${file.name} already exists`);
+
+      return true;
+    }
+
+    return false;
   };
 
   const onErrorClose = () => setError('');
 
   const dataTable = () => {
-    if (data.length > 0) {
-      return (
-        <Container className="mb-3 section">
-          <DataTable data={data} />
-        </Container>
-      );
+    if (data.length === 0) {
+      return <></>;
     }
 
-    return <></>;
+    return (
+      <Container className="mb-3 section">
+        <DataTable data={data} />
+      </Container>
+    );
+  };
+
+  const filesList = () => {
+    if (files.length === 0) {
+      return <></>;
+    }
+
+    const items = files.map((f) => <ListGroup.Item key={f}>{f}</ListGroup.Item>);
+
+    return (
+      <Container className="mb-3 section">
+        <div>Selected files:</div>
+        <ListGroup>{items}</ListGroup>
+      </Container>
+    );
   };
 
   return (
     <div className="dashboard">
       <div className="mb-3 section">
-        <CSVReader onUploadAccepted={onUploadAccepted} />
+        <CSVReader onUploadAccepted={onUploadAccepted} validator={validator} />
       </div>
+
+      {filesList()}
 
       {dataTable()}
 
